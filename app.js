69b0c17bd8a58bfdf9f06215ac51816fab10758a
choijoo <e.choijoo0228@gmail.com>
2020-04-28 23:50:39 +0800

    porgrammiin ehlel

---
+++ app.js
@@ -0,0 +1,24 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч
+var activePlayer = 1;
+
+// Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+
+// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй. 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж хадгална.
+var dice = Math.floor(Math.random() * 6) + 1;
+
+//document.querySelector("#score-0").textContent = dice;
+
+//document.querySelector("#score-1").textContent = dice;
+
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+console.log("Шоо: " + dice);
