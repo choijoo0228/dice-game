32bd16ba6ab4bf2cf1ced33b23bbf3c2eb0bec19
choijoo <e.choijoo0228@gmail.com>
2020-05-04 23:01:28 +0800

    shoo hayah

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй. 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж хадгална.
-var dice = Math.floor(Math.random() * 6) + 1;
 
 //document.querySelector("#score-0").textContent = dice;
 
 //document.querySelector("#score-1").textContent = dice;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".dice").style.display = "none";
-
-console.log("Шоо: " + dice);
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
