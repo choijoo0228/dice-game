60fc9818b00f8c59c8f33a4d99c69937c4d08cf5
choijoo <e.choijoo0228@gmail.com>
2020-05-10 19:25:12 +0800

    game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -19,9 +19,29 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх эвэнт листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 доторх санамсаргүй тоог гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+  // Шооны зургийг вэб дээр гаргаж ирнэ
   diceDom.style.display = "block";
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  // Тоглогчийн ээлжийн оноог өөрчилнө.
+  if (diceNumber !== 1) {
+    diceDom.style.display = "block";
+    roundScore += diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    scores[activePlayer] += roundScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      "" + scores[activePlayer];
+
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+  }
 });
